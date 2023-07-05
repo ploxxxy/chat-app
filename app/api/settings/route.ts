dc06441b3bc5b0743408c1ba658671fa8735a472
ploxxxy <greenenstheo@gmail.com>
2023-07-05 17:12:23 +0300

    finished user settings

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -12,8 +12,17 @@ export async function POST(request: Request) {
 			return new NextResponse('Unauthorized', { status: 401 })
 		}
 
-		const updatedUser = await prisma
-		
+		const updatedUser = await prisma.user.update({
+			where: {
+				id: currentUser.id,
+			},
+			data: {
+				image: image,
+				name: name,
+			},
+		})
+
+		return NextResponse.json(updatedUser)
 	} catch (error: any) {
 		console.log(error, 'ERROR_SETTINGS')
 		return new NextResponse('Internal Error', { status: 500 })
